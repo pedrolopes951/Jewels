54efbb7aaf9bcb343915c2c88ccd4e8dc5d5811c
pedrolopes951 <pedro.miguel.lopes951@gmail.com>
2024-01-19 12:02:12 +0000

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <SDL.h>
+
+
+int main(int argc, char* args[])
+{
+    SDL_Init(SDL_INIT_EVERYTHING);
+    std::cout << "Hello" << std::endl;
+    while (true)
+    {
+    }
+    return 0;
+}
